3786b3718aa10eeddf9d0b74f99f44dd69649cfd
James Nowek <jamesnowek@gmail.com>
2013-11-29 18:25:22 -0800

    Requests remote ASP.NET page to search for ubcids
    Instead of scraping UBC's courses page, this page now connects to a
    remote ASP.NET page on w18.ok.ubc.ca. The remote page accepts 2 GET
    parameters, pname (professor's name) and disc (discipline ie. COSC). The
    results are returned in JSON format from the ASP.NET page, and simply
    outputted on the PHP page. This allows us to perform a local domain AJAX
    request to retrieve the JSON search results.

--- getUBCID.php
+++ getUBCID.php
@@ -1,67 +1,32 @@
 <?php
-//LINK TO getSchedule.php
-$getScheduleURL = "https://studentweb.ok.ubc.ca/31317092/project/getSchedule.php";
+
+//URL to remote ASP.NET page to query SQL Server.
+$remoteSQLServerURL = "http://w18.ok.ubc.ca/api/InstructorSubjects.aspx?";
+
+//Get search terms, Professor name and Discipline.
 $pname = $_GET["pname"];
 $disc = $_GET["disc"];
 
-//Build array of POST variables to send on HTTP request
-$postVars = array(
-				'instSearchName'=>urlencode($pname),
-				'instSearchSubj'=>urlencode($disc),
-				'submit'=>urlencode('Search for Instructors'),
-				'pname'=>urlencode('instsearch'),
-				'tname'=>urlencode('instsearch')
-				);
-$fields_string = '';
-//Encodes variables according to regular post form data.
-foreach($postVars as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-rtrim($fields_string, '&');
+//Build GET parameters in request URL, according to parameters given.
+if(!empty($pname)) 	{
+	$pname = urlencode($_GET["pname"]);
+	$remoteSQLServerURL .= "pname=" . $pname;
+	if(!empty($disc))
+		$remoteSQLServerURL .= "&";
+}
+if(!empty($disc)) {
+	$disc = urlencode($_GET["disc"]);
+	$remoteSQLServerURL .= "disc=" . $disc;
+}
 
-//Use curl to post data to page
-$ch = curl_init('https://courses.students.ubc.ca/cs/main');
-curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields_string);
+//Initiate curl connection to remote page
+$ch = curl_init($remoteSQLServerURL);
 curl_setopt( $ch, CURLOPT_HEADER, 0);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($ch);
 curl_close($ch);
 
-//Load response into DOM Document Object
-$respDOM = new DOMDocument();
-$respDOM->loadHTML($response);
+header('Content-Type: application/json; charset=utf-8');
+echo $response;
 
-//Extract result table
-$mainTable = $respDOM->getElementById("mainTable");
-
-//Prepare results for output.
-header('Content-Type: application/json');
-$outputJSON = array();
-if($mainTable) {
-	//The ubcid is in the href attribute of the <a> tags
-	$results = $mainTable->getElementsByTagName("a");
-	for($i=0; $i<$results->length; $i++) {
-		$href = $results->item($i)->getAttribute("href");
-		//Perform a Regular Expression match on the href attribute to match "ubcid=(thisProfsID)"
-		if(preg_match('/ubcid=\d+/', $href, $matches)) 	{
-			$ubcidArr = explode('=', $matches[0]);
-			$ubcid = $ubcidArr[1];
-			//Make sure professor teaches at UBCO
-			$ch = curl_init($getScheduleURL.'?ubcid='.$ubcid);
-			curl_setopt( $ch, CURLOPT_HEADER, 0);
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-			$response = curl_exec($ch);
-			curl_close($ch);
-			if(count(json_decode($response, true))) {
-				$name = '';
-				//Extract innerHTML of <a> tag, which will be the professors name.
-				foreach($results->item($i)->childNodes as $node) {
-					$name .= $respDOM->saveHTML($node);
-				}
-				$outputJSON[$name] = $ubcid;
-			}
-			
-		} else echo "regex failed";
-	}
-}
-echo json_encode($outputJSON);
 ?>
