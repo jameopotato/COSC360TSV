eca0ac91dd2df92ee8f09f6b5f49b044ac6edab7
James Nowek <jamesnowek@gmail.com>
2013-11-30 10:39:56 -0800

    Simplified code and request.
    Both pname and disc parameters are sent regardless of value (including
    null/empty). Server will handle empty search terms the same as null.

--- getUBCID.php
+++ getUBCID.php
@@ -1,28 +1,15 @@
 <?php
+//Get search terms, Professor name and Discipline.
+$pname = urlencode($_GET["pname"]);
+$disc = urlencode($_GET["disc"]);
 
 //URL to remote ASP.NET page to query SQL Server.
-$remoteSQLServerURL = "http://w18.ok.ubc.ca/api/InstructorSubjects.aspx?";
-
-//Get search terms, Professor name and Discipline.
-$pname = $_GET["pname"];
-$disc = $_GET["disc"];
-
-//Build GET parameters in request URL, according to parameters given.
-if(!empty($pname)) 	{
-	$pname = urlencode($_GET["pname"]);
-	$remoteSQLServerURL .= "pname=" . $pname;
-	if(!empty($disc))
-		$remoteSQLServerURL .= "&";
-}
-if(!empty($disc)) {
-	$disc = urlencode($_GET["disc"]);
-	$remoteSQLServerURL .= "disc=" . $disc;
-}
+$remoteSQLServerURL = "http://w18.ok.ubc.ca/api/InstructorSubjects.aspx?pname=" . $pname . "&disc=" . $disc;
 
 //Initiate curl connection to remote page
 $ch = curl_init($remoteSQLServerURL);
-curl_setopt( $ch, CURLOPT_HEADER, 0);
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt( $ch, CURLOPT_HEADER, FALSE);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE);
 $response = curl_exec($ch);
 curl_close($ch);
 
