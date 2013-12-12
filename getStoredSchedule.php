a67d559e18fc7661d2ed804324ce0eb0037ac6d2
Omer <omerjavedcheema@gmail.com>
2013-12-12 14:38:15 -0800

    Retrive Course Information from the Database

--- getStoredSchedule.php
+++ getStoredSchedule.php
@@ -4,9 +4,19 @@
 	require('connect.php');
 	
 	$DB=NewADOConnection('mysql');
+	if(!$DB){
+		echo "Failed to connect to Database!";
+		exit;	
+	}
 	$DB->debug = true;
 	$DB->Connect($server, $user, $pwd, $db);
-	$dbresult = $DB->Execute("SELECT * FROM Courses WHERE ubcid = $ubcid");
-	print_r($dbresult->GetArray());
+	$dbresult = $DB->Execute("SELECT section, activity, term, days, start, end FROM Courses WHERE ubcid = $ubcid");
+	$resultArray = $dbresult->GetArray();
+	$outputJSON = array();
+	foreach ($resultArray AS $course)
+	{
+		$outputJSON[] = array("Section"=>$course['section'],"Activity"=>$course['activity'], "Term"=>$course['term'], "Days"=>$course['days'], "Start"=>$course['start'], "End"=>$course['end'])
+	}
+	echo json_encode($outputJSON);
 	$DB->Close();
 ?>
