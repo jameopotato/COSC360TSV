f5dc908d3649f2f86a909f379133fc5549358664
Omer <omerjavedcheema@gmail.com>
2013-12-12 13:27:45 -0800

    Storing Schedule in DB

--- getStoredSchedule.php
+++ getStoredSchedule.php
@@ -1,4 +1,12 @@
 <?php
-
-
+	$ubcid = $_GET['ubcid'];
+	require('adodb5/adodb.inc.php');
+	require('connect.php');
+	
+	$DB=NewADOConnection('mysql');
+	$DB->debug = true;
+	$DB->Connect($server, $user, $pwd, $db);
+	$dbresult = $DB->Execute("SELECT * FROM Courses WHERE ubcid = $ubcid");
+	print_r($dbresult->GetArray());
+	$DB->Close();
 ?>
