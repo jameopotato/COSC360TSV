8eec11126729129f9c17925411a1d42b91e270db
James Nowek <jamesnowek@gmail.com>
2013-12-12 21:03:52 -0800

    Factored all DB connection code to connect.php

--- getStoredSchedule.php
+++ getStoredSchedule.php
@@ -1,21 +1,14 @@
 <?php
 	$ubcid = $_GET['ubcid'];
-	require('adodb5/adodb.inc.php');
-	require('connect.php');
 	
-	$DB=NewADOConnection('mysql');
-	if(!$DB){
-		echo "Failed to connect to Database!";
-		exit;	
-	}
-	$DB->debug = true;
-	$DB->Connect($server, $user, $pwd, $db);
+	require('connect.php');
+
 	$dbresult = $DB->Execute("SELECT section, activity, term, days, start, end FROM Courses WHERE ubcid = $ubcid");
 	$resultArray = $dbresult->GetArray();
 	$outputJSON = array();
-	foreach ($resultArray AS $course)
+	foreach ($resultArray as $course)
 	{
-		$outputJSON[] = array("Section"=>$course['section'],"Activity"=>$course['activity'], "Term"=>$course['term'], "Days"=>$course['days'], "Start"=>$course['start'], "End"=>$course['end'])
+		$outputJSON[] = array("Section"=>$course['section'],"Activity"=>$course['activity'], "Term"=>$course['term'], "Days"=>$course['days'], "Start"=>$course['start'], "End"=>$course['end']);
 	}
 	echo json_encode($outputJSON);
 	$DB->Close();
